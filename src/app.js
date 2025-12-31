63d3c3259034eb83b6bdebf5dd4377e3eca6fcc4
SoramYO <ngoxuanson121@gmail.com>
2026-01-01 03:56:09 +0700

    feat: Implement core Fastify application setup and update Fly.io internal port to 5000.

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ const buildApp = async () => {
       }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
   });
 
